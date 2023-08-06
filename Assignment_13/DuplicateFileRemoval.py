b9b6ce0f82a595777d0af97e4005991048a0ce6d
coldbean03 <103511742+coldbean03@users.noreply.github.com>
2023-08-06 23:37:07 +0530

    Add files via upload

---
+++ Assignment_13/DuplicateFileRemoval.py
@@ -0,0 +1,138 @@
+import os
+from sys import *
+import hashlib
+import time
+import schedule
+import smtplib
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
+
+class DFR:
+    def __init__(self):
+        self.path = argv[1]
+
+    def hashfile(self,blocksize=1024):
+        path = self.path
+        fd = open(path, 'rb')
+        hasher = hashlib.md5()
+        buf = fd.read(blocksize)
+        while len(buf) > 0:
+            hasher.update(buf)
+            buf = fd.read(blocksize)
+        fd.close()
+
+        return hasher.hexdigest()
+
+    def FindDuplicate(self,path):
+        path = self.path
+        flag = os.path.isabs(self.path)
+        if flag == False:
+            path = os.path.abspath(path)
+
+        exists = os.path.isdir(path)
+
+        self.dups = {}
+
+        if exists:
+            for dirName, subDirs, fileList in os.walk(path):
+                for filen in fileList:
+                    path = os.path.join(dirName, filen)
+                    file_hash = hashfile(path)
+                    if file_hash in dups:
+                        self.dups[file_hash].append(path)
+                    else:
+                        self.dups[file_hash] = [path]
+
+            return self.dups
+        else:
+            print("Invalid Path")
+
+    def DeleteFiles(self,dict1,log_dir):
+        dict1 = self.dups
+        results = list(filter(lambda x: len(x) > 1, dict1.values()))
+
+        separator = "-" * 80
+        log_path = os.path.join(log_dir, "DuplicatesRmvLogFile.log")
+        f = open(log_path, 'w')
+        f.write(separator + "\n")
+        f.write("Directory Duplicate Files Removal Logger : " + time.ctime() + "\n")
+        f.write(separator + "\n")
+
+        iCnt = 0
+
+        if len(results) > 0:
+            f.write("Duplicates Files Removed : \n")
+            f.write("The following files were found identical : \n")
+            for result in results:
+                for subresult in result:
+                    iCnt += 1
+                    if iCnt >= 2:
+                        f.write(subresult)
+                        f.write("\n")
+                        os.remove(subresult)
+                iCnt = 0
+        else:
+            f.write("No duplicate files found")
+
+    def SendMail(self,mailid):
+        self.mailid = argv[3]
+        SMail = "inglevaibhavi03@gmail.com"
+        SPass = "****************"  # Gmail App Pass(Not Google Acc Pass)
+        RMail = self.mailid
+        mail_content = "Duplicate File Removal Information Log File"
+
+        Msg = MIMEMultipart()
+        Msg['From'] = SMail
+        Msg['To'] = RMail
+
+        Msg.attach(MIMEText(mail_content, 'plain'))
+        AFile = open(log_path, "rb")
+        payload = MIMEBase('application', 'octate-stream')
+        payload.set_payload((AFile).read())
+        encoders.encode_base64(payload)
+        payload.add_header('Content-Decomposition', "attachment; AFile= %s" % AFile)
+        Msg.attach(payload)
+
+        Session = smtplib.SMTP('smtp.gmail.com', 587)
+        Session.starttls()
+        Session.login(SMail, SPass)
+        Txt = Msg.as_string()
+        Session.sendmail(SMail, RMail, Txt)
+        Session.quit
+        print("Mail Sent")
+
+
+def main():
+    print("-----------Directory Duplicate File Removal Application-----------")
+    print("Application Name : " + argv[0])
+
+    if ((argv[1] == "-h") or (argv[1] == "-H")):
+        print("This script will delete all the duplicate files in a directory")
+        exit()
+
+    if ((argv[1] == "-u") or (argv[1]) == "-U"):
+        print("Usage : Application_name Direcory_Name")
+        exit()
+
+
+    try:
+        obj = DFR()
+        arr = {}
+        arr = obj.FindDuplicate(argv[1])
+        obj.DeleteFiles(arr,argv[1])
+        schedule.every(int(argv[2])).minutes.do(obj.SendMail,argv[3])
+
+        while (True):
+            schedule.run_pending()
+            time.sleep(1)
+
+    except ValueError:
+        print("Error : Invalid datatype of input")
+
+    except Exception as E:
+        print("Error : Invalid Input",E)
+
+if __name__ == "__main__":
+    main()
